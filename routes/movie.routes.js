9e3f7eab21629949ef73ec91764e8320e1f9a387
Chandan9574 <chandank6380@gmail.com>
2026-02-16 17:51:28 +0530

    Feat: Added get all movies api

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -23,6 +23,10 @@ const routes = (app)=>{
     app.patch('/mba/api/v1/movies/:id', // for partial update
         movieController.updateMovie
     );
+
+    app.get('/mba/api/v1/movies',
+        movieController.getMovies
+    )
 }
 
 module.exports = routes;
